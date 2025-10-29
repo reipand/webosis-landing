b4437997cbbfc862881a6236ade60752cdc422dd
Reip <113996356+reipand@users.noreply.github.com>
2025-10-29 20:42:49 +0700

    Add files via upload

---
+++ db_osissmkbi.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Oct 29, 2025 at 02:42 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_osissmkbi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+-- Error reading structure for table db_osissmkbi.admin: #1932 - Table &#039;db_osissmkbi.admin&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_osissmkbi.admin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_osissmkbi`.`admin`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_about`
+--
+-- Error reading structure for table db_osissmkbi.tb_about: #1932 - Table &#039;db_osissmkbi.tb_about&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_osissmkbi.tb_about: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_osissmkbi`.`tb_about`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_biyouth`
+--
+-- Error reading structure for table db_osissmkbi.tb_biyouth: #1932 - Table &#039;db_osissmkbi.tb_biyouth&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_osissmkbi.tb_biyouth: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_osissmkbi`.`tb_biyouth`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_divisi`
+--
+-- Error reading structure for table db_osissmkbi.tb_divisi: #1932 - Table &#039;db_osissmkbi.tb_divisi&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_osissmkbi.tb_divisi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_osissmkbi`.`tb_divisi`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_forum_aspirasi`
+--
+-- Error reading structure for table db_osissmkbi.tb_forum_aspirasi: #1932 - Table &#039;db_osissmkbi.tb_forum_aspirasi&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_osissmkbi.tb_forum_aspirasi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_osissmkbi`.`tb_forum_aspirasi`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_galeri`
+--
+-- Error reading structure for table db_osissmkbi.tb_galeri: #1932 - Table &#039;db_osissmkbi.tb_galeri&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_osissmkbi.tb_galeri: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_osissmkbi`.`tb_galeri`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_input_lomba`
+--
+-- Error reading structure for table db_osissmkbi.tb_input_lomba: #1932 - Table &#039;db_osissmkbi.tb_input_lomba&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_osissmkbi.tb_input_lomba: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_osissmkbi`.`tb_input_lomba`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_jawaban_lomba`
+--
+-- Error reading structure for table db_osissmkbi.tb_jawaban_lomba: #1932 - Table &#039;db_osissmkbi.tb_jawaban_lomba&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_osissmkbi.tb_jawaban_lomba: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_osissmkbi`.`tb_jawaban_lomba`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_main_content`
+--
+-- Error reading structure for table db_osissmkbi.tb_main_content: #1932 - Table &#039;db_osissmkbi.tb_main_content&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_osissmkbi.tb_main_content: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_osissmkbi`.`tb_main_content`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_news`
+--
+-- Error reading structure for table db_osissmkbi.tb_news: #1932 - Table &#039;db_osissmkbi.tb_news&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_osissmkbi.tb_news: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_osissmkbi`.`tb_news`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_promdat`
+--
+-- Error reading structure for table db_osissmkbi.tb_promdat: #1932 - Table &#039;db_osissmkbi.tb_promdat&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_osissmkbi.tb_promdat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_osissmkbi`.`tb_promdat`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+-- Error reading structure for table db_osissmkbi.tb_user: #1932 - Table &#039;db_osissmkbi.tb_user&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_osissmkbi.tb_user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_osissmkbi`.`tb_user`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
